193c72fcf3658e1abc865232836fa0157d23c144
githinjihans <hanson4e@gmail.com>
2023-07-06 12:11:53 +0300

    Added the remaining strings

--- src/strings.rs
+++ src/strings.rs
@@ -1,3 +1,4 @@
+// GAME24
 pub static VALUE_LAST_STEP_PROMPT: &'static str = r#"
 Use numbers and basic arithmetic operations (+ - * /) to obtain 24. Given an input and an answer, give a judgement (sure/impossible) if the answer is correct, i.e. it uses each input exactly once and no other numbers, and reach 24.
 Input: 4 4 6 8
@@ -28,7 +29,70 @@ Input: {input}
 Answer: {ans}
 Judge:"#;
 
-pub static VALUE_PROMPT: &'static str = r#"
+pub static PROPOSE_PROMPT_GAME24: &'static str = r#"
+Input: 2 8 8 14
+Possible next steps:
+2 + 8 = 10 (left: 8 10 14)
+8 / 2 = 4 (left: 4 8 14)
+14 + 2 = 16 (left: 8 8 16)
+2 * 8 = 16 (left: 8 14 16)
+8 - 2 = 6 (left: 6 8 14)
+14 - 8 = 6 (left: 2 6 8)
+14 /  2 = 7 (left: 7 8 8)
+14 - 2 = 12 (left: 8 8 12)
+Input: {input}
+Possible next steps:
+"#;
+pub static STANDARD_PROMPT_GAME24: &'static str = r#"
+Use numbers and basic arithmetic operations (+ - * /) to obtain 24.
+Input: 4 4 6 8
+Answer: (4 + 8) * (6 - 4) = 24
+Input: 2 9 10 12
+Answer: 2 * 12 * (10 - 9) = 24
+Input: 4 9 10 13
+Answer: (13 - 9) * (10 - 4) = 24
+Input: 1 4 8 8
+Answer: (8 / 4 + 1) * 8 = 24
+Input: 5 5 5 9
+Answer: 5 + 5 + 5 + 9 = 24
+Input: {input}
+"#;
+
+pub static COT_PROMPT_GAME24: &'static str = r#"
+Use numbers and basic arithmetic operations (+ - * /) to obtain 24. Each step, you are only allowed to choose two of the remaining numbers to obtain a new number.
+Input: 4 4 6 8
+Steps:
+4 + 8 = 12 (left: 4 6 12)
+6 - 4 = 2 (left: 2 12)
+2 * 12 = 24 (left: 24)
+Answer: (6 - 4) * (4 + 8) = 24
+Input: 2 9 10 12
+Steps:
+12 * 2 = 24 (left: 9 10 24)
+10 - 9 = 1 (left: 1 24)
+24 * 1 = 24 (left: 24)
+Answer: (12 * 2) * (10 - 9) = 24
+Input: 4 9 10 13
+Steps:
+13 - 10 = 3 (left: 3 4 9)
+9 - 3 = 6 (left: 4 6)
+4 * 6 = 24 (left: 24)
+Answer: 4 * (9 - (13 - 10)) = 24
+Input: 1 4 8 8
+Steps:
+8 / 4 = 2 (left: 1 2 8)
+1 + 2 = 3 (left: 3 8)
+3 * 8 = 24 (left: 24)
+Answer: (1 + 8 / 4) * 8 = 24
+Input: 5 5 5 9
+Steps:
+5 + 5 = 10 (left: 5 9 10)
+10 + 5 = 15 (left: 9 15)
+15 + 9 = 24 (left: 24)
+Answer: ((5 + 5) + 5) + 9 = 24
+Input: {input}
+"#;
+pub static VALUE_PROMPT_GAME24: &'static str = r#"
 Evaluate if given numbers can reach 24 (sure/likely/impossible)
 10 14
 10 + 14 = 24
@@ -70,6 +134,7 @@ impossible
 {input}
 "#;
 
+// CROSSWORDS
 pub static STANDARD_PROMPT_CROSSWORDS: &'static str = r#"
 Solve 5x5 mini crosswords. Given an input of 5 horizontal clues and 5 vertical clues, generate an output of 5 rows, where each row is 5 letter separated by space.
 
@@ -172,25 +237,6 @@ Input:
 {input}
 
 Output:
-"#;
-
-pub static STANDARD_PROMPT_GAME24: &'static str = r#"
-Use numbers and basic arithmetic operations (+ - * /) to obtain 24.
-Input: 4 4 6 8
-Answer: (4 + 8) * (6 - 4) = 24
-Input: 2 9 10 12
-Answer: 2 * 12 * (10 - 9) = 24
-Input: 4 9 10 13
-Answer: (13 - 9) * (10 - 4) = 24
-Input: 1 4 8 8
-Answer: (8 / 4 + 1) * 8 = 24
-Input: 5 5 5 9
-Answer: 5 + 5 + 5 + 9 = 24
-Input: {input}
-"#;
-
-pub static STANDARD_PROMPT_TEXT: &'static str = r#"
-Write a coherent passage of 4 short paragraphs. The end sentence of each paragraph must be: {input}
 "#;
 
 pub static COT_PROMPT_CROSSWORDS: &'static str = r#"
@@ -355,40 +401,68 @@ Input:
 {input}
 "#;
 
-pub static COT_PROMPT_GAME24: &'static str = r#"
-Use numbers and basic arithmetic operations (+ - * /) to obtain 24. Each step, you are only allowed to choose two of the remaining numbers to obtain a new number.
-Input: 4 4 6 8
-Steps:
-4 + 8 = 12 (left: 4 6 12)
-6 - 4 = 2 (left: 2 12)
-2 * 12 = 24 (left: 24)
-Answer: (6 - 4) * (4 + 8) = 24
-Input: 2 9 10 12
-Steps:
-12 * 2 = 24 (left: 9 10 24)
-10 - 9 = 1 (left: 1 24)
-24 * 1 = 24 (left: 24)
-Answer: (12 * 2) * (10 - 9) = 24
-Input: 4 9 10 13
-Steps:
-13 - 10 = 3 (left: 3 4 9)
-9 - 3 = 6 (left: 4 6)
-4 * 6 = 24 (left: 24)
-Answer: 4 * (9 - (13 - 10)) = 24
-Input: 1 4 8 8
-Steps:
-8 / 4 = 2 (left: 1 2 8)
-1 + 2 = 3 (left: 3 8)
-3 * 8 = 24 (left: 24)
-Answer: (1 + 8 / 4) * 8 = 24
-Input: 5 5 5 9
-Steps:
-5 + 5 = 10 (left: 5 9 10)
-10 + 5 = 15 (left: 9 15)
-15 + 9 = 24 (left: 24)
-Answer: ((5 + 5) + 5) + 9 = 24
-Input: {input}
+pub static PROPOSE_PROMPT_CROSSWORDS: &'static str = r#"Let's play a 5 x 5 mini crossword, where each word should have exactly 5 letters.
+
+{input}
+
+Given the current status, list all possible answers for unfilled or changed words, and your confidence levels (certain/high/medium/low), using the format "h1. apple (medium)". Use "certain" cautiously and only when you are 100% sure this is the correct word. You can list more then one possible answer for each word.
 "#;
+
+pub static VALUE_PROMPT_CROSSWORDS: &'static str =r#"
+Evaluate if there exists a five letter word of some meaning that fit some letter constraints (sure/maybe/impossible).
+
+Incorrect; to injure: w _ o _ g
+The letter constraint is: 5 letters, letter 1 is w, letter 3 is o, letter 5 is g.
+Some possible words that mean "Incorrect; to injure":
+wrong (w r o n g): 5 letters, letter 1 is w, letter 3 is o, letter 5 is g. fit!
+sure
+
+A person with an all-consuming enthusiasm, such as for computers or anime: _ _ _ _ u
+The letter constraint is: 5 letters, letter 5 is u.
+Some possible words that mean "A person with an all-consuming enthusiasm, such as for computers or anime":
+geek (g e e k): 4 letters, not 5
+otaku (o t a k u): 5 letters, letter 5 is u
+sure
+
+Dewy; roscid: r _ _ _ l
+The letter constraint is: 5 letters, letter 1 is r, letter 5 is l.
+Some possible words that mean "Dewy; roscid":
+moist (m o i s t): 5 letters, letter 1 is m, not r
+humid (h u m i d): 5 letters, letter 1 is h, not r
+I cannot think of any words now. Only 2 letters are constrained, it is still likely
+maybe
+
+A woodland: _ l _ d e
+The letter constraint is: 5 letters, letter 2 is l, letter 4 is d, letter 5 is e.
+Some possible words that mean "A woodland":
+forest (f o r e s t): 6 letters, not 5
+woods (w o o d s): 5 letters, letter 2 is o, not l
+grove (g r o v e): 5 letters, letter 2 is r, not l
+I cannot think of any words now. 3 letters are constrained, and _ l _ d e seems a common pattern
+maybe
+
+An inn: _ d _ w f
+The letter constraint is: 5 letters, letter 2 is d, letter 4 is w, letter 5 is f.
+Some possible words that mean "An inn":
+hotel (h o t e l): 5 letters, letter 2 is o, not d
+lodge (l o d g e): 5 letters, letter 2 is o, not d
+I cannot think of any words now. 3 letters are constrained, and it is extremely unlikely to have a word with pattern _ d _ w f to mean "An inn"
+impossible
+
+Chance; a parasitic worm; a fish: w r a k _
+The letter constraint is: 5 letters, letter 1 is w, letter 2 is r, letter 3 is a, letter 4 is k.
+Some possible words that mean "Chance; a parasitic worm; a fish":
+fluke (f l u k e): 5 letters, letter 1 is f, not w
+I cannot think of any words now. 4 letters are constrained, and it is extremely unlikely to have a word with pattern w r a k _ to mean "Chance; a parasitic worm; a fish"
+impossible
+
+{input}
+"#;
+// TEXT
+pub static STANDARD_PROMPT_TEXT: &'static str = r#"
+Write a coherent passage of 4 short paragraphs. The end sentence of each paragraph must be: {input}
+"#;
+
 
 pub static COT_PROMPT_TEXT: &'static str = r#"
 Write a coherent passage of 4 short paragraphs. The end sentence of each paragraph must be: {input}
@@ -401,24 +475,14 @@ Your plan here.
 Passage:
 Your passage here."#;
 
-pub static PROPOSE_PROMPT_CROSSWORDS: &'static str = r#"Let's play a 5 x 5 mini crossword, where each word should have exactly 5 letters.
-
-{input}
-
-Given the current status, list all possible answers for unfilled or changed words, and your confidence levels (certain/high/medium/low), using the format "h1. apple (medium)". Use "certain" cautiously and only when you are 100% sure this is the correct word. You can list more then one possible answer for each word.
+pub static VOTE_PROMPT_TEXT:&'static str = r#"
+Given an instruction and several choices, decide which choice is most promising. Analyze each choice in detail, then conclude in the last line "The best choice is {s}", where s the integer id of the choice.
 "#;
 
-pub static PROPOSE_PROMPT_GAME24: &'static str = r#"
-Input: 2 8 8 14
-Possible next steps:
-2 + 8 = 10 (left: 8 10 14)
-8 / 2 = 4 (left: 4 8 14)
-14 + 2 = 16 (left: 8 8 16)
-2 * 8 = 16 (left: 8 14 16)
-8 - 2 = 6 (left: 6 8 14)
-14 - 8 = 6 (left: 2 6 8)
-14 /  2 = 7 (left: 7 8 8)
-14 - 2 = 12 (left: 8 8 12)
-Input: {input}
-Possible next steps:
+pub static COMPARE_PROMPT_TEXT:&'static str = r#"
+Briefly analyze the coherency of the following two passages. Conclude in the last line "The more coherent passage is 1", "The more coherent passage is 2", or "The two passages are similarly coherent".
+"#;
+
+pub static SCORE_PROMPT_TEXT:&'static str = r#"
+Analyze the following passage, then at the last line conclude "Thus the coherency score is {s}", where s is an integer from 1 to 10.
 "#;
